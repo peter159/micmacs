be1e656c387000c906353c80c0cca6514c461711
peter159 <448915435@qq.com>
2019-07-28 18:10:51 +0800

    add java lsp

---
+++ lisp/init-java.el
@@ -0,0 +1,48 @@
+;;; init-java.el ---                                 -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  
+
+;; Author:  <peter.linyi@DESKTOP-PMTGUNT>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(mark-time-here)
+
+(use-package lsp-java
+  :hook (java-mode .(lambda ()
+		      (require 'lsp-java)
+		      ;; (lsp-deferred)
+		      )))
+
+(use-package mvn
+  :preface
+  (defun petmacs/mvn-clean-compile ()
+    "Recompile using maven."
+    (interactive)
+    (mvn-clean)
+    (mvn-compile)))
+
+(use-package maven-test-mode)
+
+
+(provide 'init-java)
+(message "init-java loaded in '%.2f' seconds ..." (get-time-diff time-marked))
+;;; init-java.el ends here
