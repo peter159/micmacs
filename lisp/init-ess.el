1cb2d8eae8cb0589c0a16928b1d99a0f0014bbdb
U-IPSOSGROUP\Peter.Lin <Peter.Lin@CNSH3001021.ipsosgroup.ipsos.com>
2020-03-18 16:00:08 +0800

    add r path for company computer

--- lisp/init-ess.el
+++ lisp/init-ess.el
@@ -29,7 +29,8 @@
 (use-package ess
   :ensure t
   :init
-  (setq inferior-ess-r-program "D:/R-3.5.1/bin/x64/Rterm.exe") ;inferior-R-program-name
+  ;; (setq inferior-ess-r-program "D:/R-3.5.1/bin/x64/Rterm.exe") ;inferior-R-program-name
+  (setq inferior-ess-r-program "c:/Users/peter.lin/R-3.6.2/bin/x64/Rterm.exe") ;inferior-R-program-name
   (setq ess-enable-smart-equal nil
 	ess-first-continued-statement-offset 2
 	ess-continued-statement-offset 0
