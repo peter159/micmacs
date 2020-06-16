6854f312b5e3d4db90b9a4fb504d519c1aed32f9
peter159 <peter.linyi159@gmail.com>
2020-06-16 14:47:29 +0800

    add ccls

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -67,6 +67,14 @@
   ;; :pin melpa-stable
   )
 
+(use-package ccls
+  :ensure t
+  :config
+  (setq ccls-executable "ccls")
+  (setq lsp-prefer-flymake nil)
+  (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
+  :hook ((c-mode c++-mode objc-mode) . (lambda()(require 'ccls)(lsp))))
+
 (provide 'init-lsp)
 (message "init-lsp loaded in '%.2f' seconds ..." (get-time-diff time-marked))
 ;;; init-lsp.el ends here
