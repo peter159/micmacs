0a14603251ac27aaf1b49585722b74b3de1b1fa7
peter159 <peter.linyi159@gmail.com>
2020-08-30 22:43:39 +0800

    hook golang electric spacing mode

--- lisp/init-golang.el
+++ lisp/init-golang.el
@@ -30,8 +30,6 @@
 (use-package go-mode
   :ensure t
   :init
-  (use-package electric-spacing
-    :ensure t)
   (setq gofmt-command "goimports"
 	indent-tabs-mode t)
   :functions (go-packages-gopkgs go-update-tools)
@@ -45,6 +43,7 @@
   (add-hook 'go-mode-hook
             (lambda ()
               (add-hook 'before-save-hook 'gofmt-before-save)
+	      (electric-spacing-mode t)
               (setq tab-width 4)))
   ;; Env vars
   (with-eval-after-load 'exec-path-from-shell
